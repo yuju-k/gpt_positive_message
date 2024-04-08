eb1180c5af16072631b6e9a19def4ad1e3b863dd
yuju-k <112925490+yuju-k@users.noreply.github.com>
2024-04-08 18:14:43 +0900

    위젯분리과정..

--- lib/chat/widgets/recommend_message_widget.dart
+++ lib/chat/widgets/recommend_message_widget.dart
@@ -7,17 +7,84 @@
 //   final bool isRecommendMessageWidgetVisible;
 //   final String recommandMessage;
 //   final VoidCallback onClosePressed;
-//   final VoidCallback onRecommandMessageCardTapped;
+//   final Function(String, String, String, String, String)
+//       onRecommandMessageCardTapped;
+//   final String roomId;
+//   final String myName;
+//   final String myUid;
+//   final String originalMessageContent;
+//   final String sensibility;
 
 //   const RecommendMessageWidget({
 //     required this.isRecommendMessageWidgetVisible,
 //     required this.recommandMessage,
 //     required this.onClosePressed,
 //     required this.onRecommandMessageCardTapped,
+//     required this.roomId,
+//     required this.myName,
+//     required this.myUid,
+//     required this.originalMessageContent,
+//     required this.sensibility,
 //   });
 
 //   @override
 //   Widget build(BuildContext context) {
-//     // ... (추천 메시지 위젯 구현 생략)
+//     return Visibility(
+//       visible: isRecommendMessageWidgetVisible,
+//       child: Container(
+//         width: double.infinity,
+//         color: Colors.lime[100],
+//         child: Stack(
+//           children: [
+//             RecommandMessageCard(
+//               recommandMessage: recommandMessage,
+//               onTap: () {
+//                 onRecommandMessageCardTapped(
+//                   roomId,
+//                   myName,
+//                   myUid,
+//                   originalMessageContent,
+//                   sensibility,
+//                 );
+//               },
+//             ),
+//             Positioned(
+//               right: 0,
+//               child: IconButton(
+//                 onPressed: onClosePressed,
+//                 icon: const Icon(Icons.close),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class RecommandMessageCard extends StatelessWidget {
+//   final String recommandMessage;
+//   final VoidCallback onTap;
+
+//   const RecommandMessageCard({
+//     required this.recommandMessage,
+//     required this.onTap,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       child: Card(
+//         margin: const EdgeInsets.fromLTRB(10, 10, 40, 10),
+//         child: Padding(
+//           padding: const EdgeInsets.fromLTRB(8, 3, 8, 3),
+//           child: Text(
+//             recommandMessage,
+//             style: const TextStyle(fontSize: 14),
+//           ),
+//         ),
+//       ),
+//       onTap: onTap,
+//     );
 //   }
 // }
